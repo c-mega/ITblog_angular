4504c72a9ef085c26efc0f97d808cc8d9a619ba0
Cristina <cristina.menogarcia@gmail.com>
2020-08-13 20:26:38 +0200

    Creacion de métodos en servicio

--- Blog/src/app/app.service.ts
+++ Blog/src/app/app.service.ts
@@ -12,4 +12,33 @@ export class AppService {
     this.arrPosts = [];
   }
 
+  agregarPost() { //este es un método de formularioComponent
+
+  }
+
+  getAllPosts(): Post[] {
+    return this.arrPosts;
+  }
+
+  getAllPostsPromise(): Promise<Post[]> { //cuando la promesa se resuelva devolverá un arr de posts
+    return new Promise((resolve, reject) => {
+      resolve(this.arrPosts);
+    });
+
+
+
+
+  }
+
+  getPostsByCategoria(): Promise<Post[]> { //Este es un método de blogComponent
+    return new Promise((resolve, reject) => {
+      let arrFiltrado = this.arrPosts.filter(cat => {
+        return cat.categoria;
+      });
+      resolve(arrFiltrado)
+    });
+  }
+
 }
+
+
--- Blog/src/app/blog/blog.component.ts
+++ Blog/src/app/blog/blog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
+
 
 @Component({
   selector: 'app-blog',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService: AppService) { }
 
   ngOnInit(): void {
   }
